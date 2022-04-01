dfe352d39facbd8fef91e6377013fefa0bd08878
George Wiseman <georgeawiseman@gmail.com>
2022-04-01 14:38:20 -0700

    Documentation and More Css added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import Gallery from './Gallery.js'
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-import './App.css'
+import Gallery from './Gallery.js' 
+import React, { useState, useEffect } from 'react'; 
+import axios from 'axios' // Fetching
+import './App.css' // Styling
 
+// An imported function, for easy shuffling an array
 var shuffleArr = require('shuffle-array')
 
-
+// Where to store the local data for the photos list
 const LOCAL_KEY = "photos_key";
 
 function App() {
 
+  //photos will be a list of objects containing titles
+  //urls and ids. Whenever this state changes, this will 
+  // cause react to render the components that need to be
+  // changed. 
   const [photos, setPhotos] = useState([]);
 
+
   useEffect(() => {
-    // Load the photos in for the first time? Or check if they are already in local storage?
+    /*
+    This use effect will trigger on startup and only once
+    It should try to read from local storage to see if there
+    is a list of photos there. If there is,
+    it will set the new state of photos to what was retrieved from local
+    storage. 
+    Otherise, it should fetch a list of 2 photos from JSON-placeholder
+    and set the state of photos to this list. 
+     */
     const localPhotos = JSON.parse(localStorage.getItem(LOCAL_KEY))
-    if(localPhotos===undefined || 
-        localPhotos.length != 0) {
+    console.log(localPhotos);
+    if(localPhotos!==null) {
       console.log("Local Storage found")
       console.log(localPhotos)
       setPhotos(localPhotos)
     }
     else{
       console.log("fetching")
+      // Only fetch keep a small number of the photos 
+      // 500+ didn't look good on the app
       axios.get("https://jsonplaceholder.typicode.com/photos")
       .then(resp => setPhotos(resp.data.slice(1,20)))
     }
   }, [])
 
+
+  // This use effect will trigger every time the photos state changes
+  // and will cause the photos list to be stored into local storage
+  // so that fetching is not required on a refresh of the page.
   useEffect( () => {
     console.log("Storing state in local")
     localStorage.setItem(LOCAL_KEY, JSON.stringify(photos));
@@ -44,10 +64,16 @@ function App() {
   }
 
   return (
+    // Now render the app, with a Gallery and a button.
+    // Gallery should be passed the state-variable photos
+    // as its propery 
     <div className="App">
+      Art Gallery: By George Wiseman
       <Gallery photoList={photos}/>
-      <button onClick={arrangeClick}>
-        Rearange Photo Gallery
+      <button 
+      className='button'
+      onClick={arrangeClick}>
+        Rearange Gallery
       </button>
     </div>
   );
--- src/Gallery.js
+++ src/Gallery.js
@@ -2,20 +2,29 @@ import React from 'react'
 import Photo from './Photo';
 import HorizontalScroller from 'react-horizontal-scroll-container'
 import './Gallery.css'
- export default function Gallery({ photoList }) {
-  if (photoList[0] == undefined){
+
+// A gallery will be a collection of Photo Components, rendered
+// with a Horizontal Scroll. 
+export default function Gallery({ photoList }) {
+  if (photoList === undefined){
+    // If there are no photos loaded yet, it shouldn't try to access
+    // anything.
     return(
     <div>Loading</div>);
   }
   else{
-    const photos = photoList.map( photo => 
+    // Map out all the photos in JSX tags so that it can 
+    // all be rendered when returning. Passing url, title 
+    // and making sure to give each photo a key, so React 
+    // can re-render more efficiently  
+    const photosRender = photoList.map( photo => 
       <Photo key={photo.id} 
         url={photo.url}
         name={photo.title}/>)
    return (
      <div className="Gallery">
       <HorizontalScroller>
-        {photos}
+        {photosRender}
       </HorizontalScroller>
      </div>
    );
